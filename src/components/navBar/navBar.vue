66ed2f0cf1c7755f3041f7f867eeaf304364928d
zhongxinzhi <1123858432@qq.com>
2017-04-10 19:33:32 +0800

    查看bug

--- src/components/navBar/navBar.vue
+++ src/components/navBar/navBar.vue
@@ -18,7 +18,19 @@
           <ul>
               <li v-for="navItem in navBar">
                   <router-link :to="{ name: navItem.name}" :class="{ checked: navChecked[navItem.name] }" v-if="navItem.isRouterLink">{{ navItem.title}}</router-link>
-                  <a :href="navItem.link" v-else>{{ navItem.name}}</a>
+                  <template v-else>
+                      <div style="position: relative" @click.stop>
+                          <a href="javascript:void(0)">{{ navItem.title}}{{show.navContent[navItem.name]}}<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                              <div class="list animated" @click="show.navContent[navItem.name]=!show.navContent[navItem.name]">
+                                  <ul>
+                                      <transition name="list-toggle" v-for="n in 5">
+                                        <li v-show="show.navContent[navItem.name]" class="animated" :class="{['enter-delay-'+ n]: show.searchList,['leave-delay-'+ n]: !show.searchList}">test</li>
+                                      </transition>
+                                  </ul>
+                              </div>
+
+                      </div>
+                  </template>
               </li>
           </ul>
       </div>
@@ -42,10 +54,19 @@
         }
         .nav-content{
             float: right;
-            ul{
+            .list{
+                li{
+                    display: block;
+                    margin: 0;
+                }
+                :last-child{
+                    margin: 0;
+                }
+            }
+            &>ul{
                 font-size: 0;
                 height: 100%;
-                li{
+                &>li{
                     display: inline-block;
                     font-size: 15px;
                     margin-right: 30px;
@@ -92,7 +113,7 @@
                 border-radius: 10px;
                 outline: none;
                 border: none;
-                height: 30px;
+                height: 32px;
                 padding: 0 25px 0 15px;
             }
             i{
@@ -105,33 +126,24 @@
                     color: darken(@font-color,20%);
                 }
             }
-            .list{
-                ul{
-                    position: relative;
-                    top: -5px;
-                    text-align: center;
-                    /*&:before{*/
-                        /*content: '';*/
-                        /*width: 0;*/
-                        /*height: 0;*/
-                        /*border-width: 0 10px 10px 10px;*/
-                        /*border-style: solid;*/
-                        /*border-color: transparent transparent #191919 transparent;*/
-                        /*position: absolute;*/
-                        /*top: -9px;*/
-                        /*left: 50%;*/
-                        /*transform: translateX(-50%);*/
-                    /*}*/
-                }
-                li{
-                    background: #191919;
-                    height: 30px;
-                    line-height: 30px;
-                    border-top: 1px solid #202020;
-                    border-bottom: 1px solid #0c0c0c;
-                }
-
+        }
+        .list{
+            ul{
+                position: absolute;
+                top: 60px;
+                left: 50%;
+                transform: translateX(-50%);
+                text-align: center;
+                width: 150px;
             }
+            li{
+                background: #191919;
+                height: 32px;
+                line-height: 32px;
+                border-top: 1px solid #202020;
+                border-bottom: 1px solid #0c0c0c;
+            }
+
         }
     }
     .generate-enter-delay(5);
@@ -169,10 +181,23 @@
                        title: '首页',
                        link: './index',
                        isRouterLink: true,
+                   },
+                   {
+                       name: 'index',
+                       title: '首页',
+                       link: './main',
+                       isRouterLink: true,
+                   },
+                   {
+                       name: 'expand',
+                       title: '拓展',
                    }
                ],
                 show:{
-                    searchList: false
+                    searchList: false,
+                    navContent:{
+                        expand: false
+                    }
                 }
             }
         },
@@ -181,9 +206,11 @@
             setFalse: function (obj) {
 
                 for(let key in obj){
-                    if(obj.key instanceof Object){
+                    if(typeof obj.key == Object){
+                        debugger
                         arguments.callee(obj[key])
                     }else{
+                        debugger
                         obj[key] = false
                     }
 
