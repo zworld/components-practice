da9df32cb19e326ef7e988dbd01f87a24b4b71f3
zhongxinzhi <1123858432@qq.com>
2017-04-15 14:54:27 +0800

    轮播

--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -24,7 +24,16 @@
         },
 
         mounted(){
-            this.mySwipe = Swipe(document.getElementById('slider'));
+            window.mySwipe = new Swipe(document.getElementById('slider'), {
+                startSlide: 2,
+                speed: 400,
+                auto: 3000,
+                continuous: true,
+                disableScroll: false,
+                stopPropagation: false,
+                callback: function(index, elem) {},
+                transitionEnd: function(index, elem) {}
+            });
         },
         computed: {
 
